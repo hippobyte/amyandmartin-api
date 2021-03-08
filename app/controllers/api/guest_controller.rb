e65da442f0bbbf70b08edf76a47fb37ab8df0f04
roughcompass <martinmarzejon@gmail.com>
2021-03-08 14:24:30 -0800

    progress

--- app/controllers/api/guest_controller.rb
+++ app/controllers/api/guest_controller.rb
@@ -7,16 +7,18 @@ module Api
 
     def create
       Guest.first.update(options: @data)
+
+      render json: { data: @data, guest_params: guest_params }
     end
 
     private
 
     def guest_params
-      params.permit(data: {})
+      params.permit(:data)
     end
 
     def set_data
-      @data = guest_params[:data]
+      @data = params
     end
   end
 end
