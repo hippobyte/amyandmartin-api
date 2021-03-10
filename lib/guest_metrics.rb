49add535c13f60692017c29081b6635b50606e82
roughcompass <martinmarzejon@gmail.com>
2021-03-10 12:08:17 -0800

    counts

--- lib/guest_metrics.rb
+++ lib/guest_metrics.rb
@@ -19,24 +19,36 @@ class GuestMetrics
         first_name: guest.first_name,
         last_name: guest.last_name,
         email: email(guest.guest_contacts),
-        adults: 0,
-        children: 0,
-        total: 0,
-        rsvp_date: guest.rsvp.confirmed? ? guest.rsvp.updated_at : nil
-      }.merge(counts(guest.rsvp))
+        adults: rsvp_adult_count(guest),
+        children: rsvp_children_count(guest),
+        total: rsvp_total_count(guest),
+        rsvp_date: rsvp_date(guest.rsvp)
+      }
     end
   end
 
   private
 
-  def counts(rsvp)
-    return {} unless rsvp.confirmed?
+  def rsvp_adult_count(guest)
+    return guest.guest_count.to_i + 1 unless guest.rsvp.confirmed?
 
-    {
-      adults: rsvp.guest_count.to_i,
-      children: rsvp.children_count.to_i,
-      total: rsvp.guest_count.to_i + rsvp.children_count.to_i
-    }
+    guest.rsvp.guest_count.to_i
+  end
+
+  def rsvp_children_count(guest)
+    return guest.children_count.to_i unless guest.rsvp.confirmed?
+
+    guest.rsvp.children_count.to_i
+  end
+
+  def rsvp_total_count(guest)
+    rsvp_adult_count(guest) + rsvp_children_count(guest)
+  end
+
+  def rsvp_date(rsvp)
+    return unless rsvp.confirmed?
+
+    rsvp.updated_at.in_time_zone('America/Los_Angeles').strftime('%Y-%m-%d')
   end
 
   def email(guest_contacts)
