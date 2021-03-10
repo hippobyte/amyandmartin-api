25d71e565cf4510e1c3b9b4286cd3a5cad54c920
roughcompass <martinmarzejon@gmail.com>
2021-03-10 11:18:55 -0800

    progress

--- lib/guest_metrics.rb
+++ lib/guest_metrics.rb
@@ -15,18 +15,27 @@ class GuestMetrics
   def summary
     @guests.map do |guest|
       {
-        response:   guest.rsvp.status,
+        response: guest.rsvp.status,
         first_name: guest.first_name,
-        last_name:  guest.last_name,
-        email:      email(guest.guest_contacts),
-        adults:     guest.rsvp.guest_count.to_i,
-        children:   guest.rsvp.children_count.to_i,
-        total:      guest.rsvp.guest_count.to_i + guest.rsvp.children_count.to_i
-      }
+        last_name: guest.last_name,
+        email: email(guest.guest_contacts)
+      }.merge(counts(guest.rsvp))
     end
   end
 
   private
+
+  def counts(rsvp)
+    if rsvp.confirmed?
+      return {
+        adults: rsvp.guest_count.to_i,
+        children: rsvp.children_count.to_i,
+        total: rsvp.guest_count.to_i + rsvp.children_count.to_i
+      }
+    end
+
+    { adults: 0, children: 0, total: 0 }
+  end
 
   def email(guest_contacts)
     email = guest_contacts.map(&:email)
